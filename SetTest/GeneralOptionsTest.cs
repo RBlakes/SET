e3bbb6ef12d62dd644668a1e8c9e24dd53294499
Randy <randy.blakeslee@aggiemail.usu.edu>
2015-11-19 12:40:46 -0700

    Added GeneralOptions unit test

---
+++ SetTest/GeneralOptionsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetTest
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    class GeneralOptionsTest
+    {
+    }
+}
